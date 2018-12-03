96aeca2bb1a28b97ec318839bfccbfe0611b8a32
Dorian Henning <dorian.henning@gmail.com>
2018-12-03 18:35:20 +0000

    os push

--- se_denseslam/src/object/Object.cpp
+++ se_denseslam/src/object/Object.cpp
@@ -51,6 +51,73 @@ void Object::integrateBackgroundKernel(const Eigen::Vector4f    k,
                                        const Eigen::Vector2i    frameSize)
 {
     // TODO: implement this
+    bool doIntegrate = checkPoseKernel(pose_, old_pose_,
+                                       reduction_output_.data(),
+                                       computation_size_, track_threshold);
+
+    if ((doIntegrate && ((frame % integration_rate) == 0)) || (frame <= 3)) {
+
+        float voxelsize =  volume_._dim/volume_._size;
+        int num_vox_per_pix = volume_._dim/((se::VoxelBlock<FieldType>::side)*voxelsize);
+        size_t total = num_vox_per_pix * computation_size_.x() * computation_size_.y();
+        allocation_list_.reserve(total);
+
+        unsigned int allocated = 0;
+        if(std::is_same<FieldType, SDF>::value) {
+            allocated  = buildAllocationList(allocation_list_.data(),
+                                             allocation_list_.capacity(),
+                                             *volume_._map_index, pose_, getCameraMatrix(k), float_depth_.data(),
+                                             computation_size_, volume_._size,
+                                             voxelsize, 2*mu);
+        } else if(std::is_same<FieldType, OFusion>::value) {
+            allocated = buildOctantList(allocation_list_.data(), allocation_list_.capacity(),
+                                        *volume_._map_index,
+                                        pose_, getCameraMatrix(k), float_depth_.data(), computation_size_, voxelsize,
+                                        compute_stepsize, step_to_depth, 6*mu);
+        }
+
+        volume_._map_index->allocate(allocation_list_.data(), allocated);
+
+        if(std::is_same<FieldType, SDF>::value) {
+            if (render_color_) {
+                struct sdf_update funct(float_depth_.data(), float_rgb_.data(),
+                                        Eigen::Vector2i(computation_size_.x(), computation_size_.y()), mu, 100);
+                se::functor::projective_map(*volume_._map_index,
+                                            Sophus::SE3f(pose_).inverse(),
+                                            getCameraMatrix(k),
+                                            Eigen::Vector2i(computation_size_.x(), computation_size_.y()),
+                                            funct);
+            } else {
+                struct sdf_update funct(float_depth_.data(),
+                                        Eigen::Vector2i(computation_size_.x(), computation_size_.y()), mu, 100);
+                se::functor::projective_map(*volume_._map_index,
+                                            Sophus::SE3f(pose_).inverse(),
+                                            getCameraMatrix(k),
+                                            Eigen::Vector2i(computation_size_.x(), computation_size_.y()),
+                                            funct);
+            }
+
+        } else if(std::is_same<FieldType, OFusion>::value) {
+
+            float timestamp = (1.f/30.f)*frame;
+            struct bfusion_update funct(float_depth_.data(),
+                                        Eigen::Vector2i(computation_size_.x(), computation_size_.y()), mu, timestamp);
+
+            se::functor::projective_map(*volume_._map_index,
+                                        Sophus::SE3f(pose_).inverse(),
+                                        getCameraMatrix(k),
+                                        Eigen::Vector2i(computation_size_.x(), computation_size_.y()),
+                                        funct);
+        }
+
+        doIntegrate = true;
+    } else {
+        doIntegrate = false;
+    }
+
+    return doIntegrate;
+
+}
 }
 
 void Object::integrateVolumeKernel()
